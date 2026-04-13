31d39f76f1a29aab91df8212afe6afe35e7f5eab
GustavoToaiari <gustavo.toaiari@gmail.com>
2026-04-13 16:36:19 -0300

    3 ePuck's to Goal
    Faz os três robôs irem para o Goal. Próximo passo é a formação em triângulo.

--- src/triangulo/main.py
+++ src/triangulo/main.py
@@ -8,11 +8,22 @@ client = RemoteAPIClient()
 sim = client.getObject('sim')
 client.setStepping(True)
 
-robot = Robot.Robot(name= 'ePuck',
-              base=sim.getObject('/base'),
+# Ver formas de melhorar isso, para 3 robôs esta "ok". Mas, quando escalar para mais robôs, ficará inviável essa repetição de Robot.Robot
+robots = [Robot.Robot(name= 'ePuck1',
+              base=sim.getObject('/base1'),
               goal_path=sim.getObject('/Goal'),
-              wheel_path=(sim.getObject('/leftJoint'), sim.getObject('/rightJoint')),
-              sim=sim)
+              wheel_path=(sim.getObject('/leftJoint1'), sim.getObject('/rightJoint1')),
+              sim=sim),
+        Robot.Robot(name= 'ePuck2',
+              base=sim.getObject('/base2'),
+              goal_path=sim.getObject('/Goal'),
+              wheel_path=(sim.getObject('/leftJoint2'), sim.getObject('/rightJoint2')),
+              sim=sim),
+        Robot.Robot(name= 'ePuck3',
+              base=sim.getObject('/base3'),
+              goal_path=sim.getObject('/Goal'),
+              wheel_path=(sim.getObject('/leftJoint3'), sim.getObject('/rightJoint3')),
+              sim=sim)]
 
 if sim.getSimulationState() == sim.simulation_stopped:
         sim.startSimulation()
@@ -22,16 +33,20 @@ start_sim = sim.getSimulationTime()
 while True:
     client.step()
 
-    arrived = robot.run()
+    arrived = [] # Guarda se cada robô chegou no goal
+    for robot in robots:
+         arrived.append(robot.run())
 
-    if arrived:
-        robot.stop_robot()
-        print("Robô chegou no goal.")
+    if all(arrived): # Se todos os elementos da lista arrived forem verdadeiros, significa que todos chegaram no Goal
+        for robot in robots:
+            robot.stop_robot()
+        print("Todos robôs chegaram no Goal.")
         sim.stopSimulation()
         break
 
-    if sim.getSimulationTime() - start_sim > 60:
-        robot.stop_robot()
+    if sim.getSimulationTime() - start_sim > 60: # Critério de segurança, encerra se passar de 60 segundos de simulação
+        for robot in robots:
+            robot.stop_robot()
         print("Timeout. Encerrando.")
         sim.stopSimulation()
         break
